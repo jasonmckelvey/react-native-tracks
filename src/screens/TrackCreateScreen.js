1f006f12197e9d962a63d87b129f6bb64b78a32b
Jason <jason.mckelvey@companycam.com>
2019-10-04 11:13:39 -0500

    added permission prompts to request user location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Map from '../components/Map';
+import {requestPermissionsAsync} from 'expo-location';
 import styled from 'styled-components';
 
 const Header = styled.Text`
@@ -9,6 +10,13 @@ const Header = styled.Text`
   margin: 48px 0px 48px 0px;
   color: #252a31;
 `;
+const Error = styled.Text`
+  font-size: 16px;
+  font-weight: 500;
+  letter-spacing: -0.25px;
+  color: #d21c1c;
+`;
+
 const Wrapper = styled.View`
   padding: 16px;
   flex: 1;
@@ -18,11 +26,26 @@ const Safe = styled.SafeAreaView`
 `;
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWacthing = async () => {
+    try {
+      await requestPermissionsAsync();
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWacthing();
+  }, []);
+
   return (
     <Wrapper>
       <Safe>
         <Header>Create a Track</Header>
         <Map />
+        {err ? <Error>Please enable location services</Error> : null}
       </Safe>
     </Wrapper>
   );
